499419e7b6ab350eb591c2563b2011270c2849ab
devIncogni <arinkumar10@outlook.com>
2024-07-27 15:24:55 +0530

    Rough functionality added to card addition

--- script.js
+++ script.js
@@ -70,10 +70,69 @@ console.log(myLibrary);
 // #region Add DOM card elements
 
 const mainContainer = document.querySelector("#mainContainer");
+
 function createCard() {
   let card = document.createElement("div");
   card.classList.add("card");
-  mainContainer.append(card);
+  card.setAttribute("data-index", myLibrary.length - 1);
+
+  let divList = createDiv([
+    "titleHolder",
+    "authorHolder",
+    "pageNumber",
+    "readingStatus",
+    "btnsHolder",
+  ]);
+
+  giveContent(divList, myLibrary.length - 1);
+
+  let btnDivList = createDiv(["delCard", "toggleRead"]);
+  btnDivList[0].textContent = "Delete";
+
+  if (divList[3].textContent == "false") {
+    btnDivList[1].textContent = "Mark Read";
+    divList[3].textContent = "Not Read";
+  } else {
+    btnDivList[1].textContent = "Didn't Read";
+    divList[3].textContent = "Read";
+  }
+
+  btnDivList.forEach((element) => {
+    divList[divList.length - 1].appendChild(element);
+  });
+
+  divList.forEach((element) => {
+    card.appendChild(element);
+  });
+
+  mainContainer.appendChild(card);
+}
+
+function createDiv(classNameArray) {
+  let divElement = [];
+  let cardChild;
+  for (let i = 0; i < classNameArray.length; i++) {
+    cardChild = document.createElement("div");
+    cardChild.classList.add(classNameArray[i]);
+
+    divElement.push(cardChild);
+  }
+
+  return divElement;
+  // document.createElement('div');
+}
+
+function giveContent(divList, index) {
+  let j = 0;
+  for (const key in myLibrary[index]) {
+    if (Object.hasOwnProperty.call(myLibrary[index], key)) {
+      divList[j].textContent = myLibrary[index][key];
+      if ((j == 2)) {
+        divList[j].textContent += " Pages";
+      }
+      j++;
+    }
+  }
 }
 
 // #endregion Add DOM card elements
