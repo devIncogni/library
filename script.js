2222e27c05b1792ed9fa1ac42c1060221e6441be
devIncogni <arinkumar10@outlook.com>
2024-12-17 19:08:28 +0530

    Styled main content and Scrollbar and added functionality to add book button.

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ function Book(name, author, pages, readStatus) {
 function addBookToLib(name, author, pages, readStatus) {
   bookLib.push(new Book(name, author, pages, readStatus));
 }
+
+// #region Dialog functionality
+
+const dialog = document.querySelector("dialog");
+const closeBtn = document.querySelector("#close");
+
+closeBtn.addEventListener("click", (event) => {
+  dialog.close();
+  // alert("close");
+  event.preventDefault();
+});
+
+const openBtn = document.querySelector("#addBook");
+
+openBtn.addEventListener("click", (event) => {
+  dialog.showModal();
+});
+
+
+
+// #endregion Dialog functionality
