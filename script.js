f2f32296320aee81cb1835b8e7758b0a27079cc2
devIncogni <arinkumar10@outlook.com>
2024-12-18 12:15:48 +0530

    Added functionality to dialog box form to generate and push a book object in the array

--- script.js
+++ script.js
@@ -13,21 +13,65 @@ function addBookToLib(name, author, pages, readStatus) {
 
 // #region Dialog functionality
 
-const dialog = document.querySelector("dialog");
+function openDialogModal(dialog) {
+  dialog.showModal();
+}
+
+function closeDialogModal(dialog) {
+  dialog.close();
+}
+
+function resetForm(form) {
+  form.reset();
+}
+
+function extreactDataFromForm(form) {
+  let bookName = form.querySelector("#bookName").textContent;
+  let authorName = form.querySelector("#authorName").textContent;
+  let pageNum = form.querySelector("#pageNum").textContent;
+  let readStatus =
+    form.querySelector("#read").checked == true ? "Read" : "Not Read";
+
+  return { bookName, authorName, pageNum, readStatus };
+}
+
 const closeBtn = document.querySelector("#close");
+const openBtn = document.querySelector("#addBook");
+const subBtn = document.querySelector("#submit");
+const resetBtn = document.querySelector("#resetBtn");
 
 closeBtn.addEventListener("click", (event) => {
-  dialog.close();
-  // alert("close");
+  const dialog = document.querySelector("dialog");
   event.preventDefault();
+  closeDialogModal(dialog);
 });
-
-const openBtn = document.querySelector("#addBook");
 
 openBtn.addEventListener("click", (event) => {
-  dialog.showModal();
+  const dialog = document.querySelector("dialog");
+  openDialogModal(dialog);
 });
 
+resetBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  resetForm(document.querySelector(".dialogForm"));
+});
 
+subBtn.addEventListener("click", (event) => {
+  // event.preventDefault();
+  let form = document.querySelector(".dialogForm");
+  if (form.reportValidity() == false) {
+    return;
+  }
+  let bookData = extreactDataFromForm(document);
+
+  addBookToLib(
+    bookData.bookName,
+    bookData.authorName,
+    bookData.pageNum,
+    bookData.readStatus
+  );
+
+  console.log(bookData);
+});
 
 // #endregion Dialog functionality
